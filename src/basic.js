4eb46de6c578aecdb3d5ac90abe6a5262a70abba
Madnikulin50 <madnikulin50@gmail.com>
2017-06-23 23:18:23 +0300

    commit

---
+++ src/basic.js
@@ -0,0 +1,22 @@
+
+class BasicAlgorithm
+{
+	constructor(in_Params)
+	{
+
+	}
+
+	train(in_Params, in_CB)
+	{
+		throw "Недописано";
+	}
+
+	check(in_Params, in_CB)
+	{
+		throw "Недописано";
+	}
+
+
+};
+
+module.exports = BasicAlgorithm;
